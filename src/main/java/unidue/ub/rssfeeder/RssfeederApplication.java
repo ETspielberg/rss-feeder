252c5f5cef2400f5d272ec9235883e68c50cb4c0
Eike Spielberg <Eike.Spielberg@uni-due.de>
2018-01-31 16:01:07 +0100

    adding eureka

--- src/main/java/unidue/ub/rssfeeder/RssfeederApplication.java
+++ src/main/java/unidue/ub/rssfeeder/RssfeederApplication.java
@@ -2,8 +2,10 @@ package unidue.ub.rssfeeder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RssfeederApplication {
 
 	public static void main(String[] args) {
