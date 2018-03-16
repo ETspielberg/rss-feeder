471c8c6778a9744687ed1077486bc800df8594bd
Eike Spielberg <Eike.Spielberg@uni-due.de>
2018-03-16 18:33:26 +0100

    changed Ignored to analysis package

--- src/main/java/unidue/ub/rssfeeder/RssFeederController.java
+++ src/main/java/unidue/ub/rssfeeder/RssFeederController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import unidue.ub.media.analysis.Ignored;
+import unidue.ub.media.blacklist.Ignored;
 import unidue.ub.media.analysis.Nrequests;
 import unidue.ub.settings.fachref.Alertcontrol;
 import unidue.ub.settings.fachref.Notationgroup;
