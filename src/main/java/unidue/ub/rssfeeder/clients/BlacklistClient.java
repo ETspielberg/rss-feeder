2c440c5ea3bd2e4709f1769d7d9d55e227cf7b94
Eike Spielberg <Eike.Spielberg@uni-due.de>
2019-01-28 19:34:22 +0100

    adding Requestparam

--- src/main/java/unidue/ub/rssfeeder/clients/BlacklistClient.java
+++ src/main/java/unidue/ub/rssfeeder/clients/BlacklistClient.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @FeignClient(name="blacklist-backend", configuration = FeignConfiguration.class)
@@ -12,6 +13,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public interface BlacklistClient {
 
     @RequestMapping(method= RequestMethod.GET, value="/getIgnoredFor/{identifier}")
-    Boolean isBlocked(@PathVariable("identifier") String identifier, String type);
+    Boolean isBlocked(@PathVariable("identifier") String identifier, @RequestParam("type") String type);
 
 }
